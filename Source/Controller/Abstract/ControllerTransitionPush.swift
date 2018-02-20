af2f97b01d049aa7f87665e9d9e4c9fd874c1258
Zero <zero.griffin@gmail.com>
2018-02-20 08:38:36 +0100

    Add properties to controller transition struct

--- Source/Controller/Abstract/ControllerTransitionPush.swift
+++ Source/Controller/Abstract/ControllerTransitionPush.swift
@@ -2,5 +2,21 @@ import UIKit
 
 struct ControllerTransitionPush
 {
+    var completion:(() -> ())?
+    var left:CGFloat
+    var top:CGFloat
+    var background:Bool
+    let controller:UIViewController
+    let newView:UIView
     
+    init(
+        controller:UIViewController,
+        newView:UIView)
+    {
+        self.controller = controller
+        self.newView = newView
+        self.top = 0
+        self.left = 0
+        self.background = false
+    }
 }
