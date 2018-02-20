ed91d02ece835c37d76bb2d972f0c27812a5030e
Zero <zero.griffin@gmail.com>
2018-02-20 16:53:38 +0100

    Layout equals view

--- Source/Extensions/ExtensionUIView+ConstraintsEquals.swift
+++ Source/Extensions/ExtensionUIView+ConstraintsEquals.swift
@@ -3,4 +3,53 @@ import UIKit
 extension UIView
 {
     //MARK: internal
+    
+    func layoutEquals(
+        toView:UIView,
+        margin:CGFloat = 0)
+    {
+        NSLayoutConstraint.topToTop(
+            view:view,
+            toView:toView,
+            constant:margin)
+        NSLayoutConstraint.bottomToBottom(
+            view:view,
+            toView:toView,
+            constant:-margin)
+        NSLayoutConstraint.leftToLeft(
+            view:view,
+            toView:toView,
+            constant:margin)
+        NSLayoutConstraint.rightToRight(
+            view:view,
+            toView:toView,
+            constant:-margin)
+    }
+    
+    func layoutEqualsHorizontal(
+        toView:UIView,
+        margin:CGFloat = 0)
+    {
+        NSLayoutConstraint.leftToLeft(
+            view:view,
+            toView:toView,
+            constant:margin)
+        NSLayoutConstraint.rightToRight(
+            view:view,
+            toView:toView,
+            constant:-margin)
+    }
+    func layoutEqualsVertical(
+        toView:UIView,
+        margin:CGFloat = 0)
+    {
+        NSLayoutConstraint.topToTop(
+            view:view,
+            toView:toView,
+            constant:margin)
+        NSLayoutConstraint.bottomToBottom(
+            view:view,
+            toView:toView,
+            constant:-margin)
+    }
 }
