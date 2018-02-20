320f9f12436a3c66a63e7adbecf3f53e81318482
Zero <zero.griffin@gmail.com>
2018-02-20 14:02:51 +0100

    Refactor view pop

--- Source/View/Global/View/ViewParentPopTransition.swift
+++ Source/View/Global/View/ViewParentPopTransition.swift
@@ -2,5 +2,15 @@ import UIKit
 
 struct ViewParentPopTransition
 {
+    var completion:(() -> ())?
+    var left:CGFloat
+    var top:CGFloat
+    let currentView:ViewTransitionableProtocol
     
+    init(currentView:ViewTransitionableProtocol)
+    {
+        self.currentView = currentView
+        self.left = 0
+        self.top = 0
+    }
 }
