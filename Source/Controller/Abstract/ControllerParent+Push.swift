4f04c644b95879cd71fd43f97c61b7fa87a73a90
Zero <zero.griffin@gmail.com>
2018-02-20 08:35:39 +0100

    Create struct controller transition push

--- Source/Controller/Abstract/ControllerParent+Push.swift
+++ Source/Controller/Abstract/ControllerParent+Push.swift
@@ -36,6 +36,22 @@ extension ControllerParent
     
     //MARK: internal
     
+    func mainController(controller:UIViewController)
+    {
+        self.addChildViewController(controller)
+        
+        guard
+            
+            let newView:ViewTransitionableProtocol = controller.view as? ViewTransitionableProtocol
+            
+        else
+        {
+            return
+        }
+        
+        self.viewParent?.mainView(view:newView)
+    }
+    
     func slideTo(
         transition:ControllerTransition,
         controller:UIViewController)
@@ -46,5 +62,79 @@ extension ControllerParent
         self.slide(
             controller:controller,
             left:left)
+    }
+    
+    
+    func push(
+        controller:UIViewController,
+        transition:ControllerTransition,
+        background:Bool = true,
+        completion:(() -> ())? = nil)
+    {
+        guard
+            
+            let viewParent:ViewParent = self.viewParent,
+            let newView:ViewTransitionableProtocol = controller.view as? ViewTransitionableProtocol
+            
+        else
+        {
+            return
+        }
+        
+        let width:CGFloat = viewParent.bounds.maxX
+        let height:CGFloat = viewParent.bounds.maxY
+        let left:CGFloat = width * transition.horizontal
+        let top:CGFloat = height * transition.vertical
+        
+        self.pushController(
+            controller:controller,
+            newView:newView,
+            left:left,
+            top:top,
+            background:background,
+            completion:completion)
+    }
+    
+    func animateOver(controller:UIViewController)
+    {
+        guard
+            
+            let newView:ViewTransitionableProtocol = controller.view as? ViewTransitionableProtocol
+            
+        else
+        {
+            return
+        }
+        
+        self.addChildViewController(controller)
+        controller.beginAppearanceTransition(true, animated:true)
+        self.currentController?.beginAppearanceTransition(false, animated:true)
+        
+        self.viewParent?.animateOver(newView:newView)
+        { [weak self] in
+            
+            controller.endAppearanceTransition()
+            self?.currentController?.endAppearanceTransition()
+        }
+    }
+    
+    func centreOver(controller:UIViewController)
+    {
+        guard
+            
+            let newView:ViewTransitionableProtocol = controller.view as? ViewTransitionableProtocol
+            
+        else
+        {
+            return
+        }
+        
+        self.addChildViewController(controller)
+        controller.beginAppearanceTransition(true, animated:true)
+        self.currentController?.beginAppearanceTransition(false, animated:true)
+        self.viewParent?.centreOver(newView:newView)
+        
+        controller.endAppearanceTransition()
+        self.currentController?.endAppearanceTransition()
     }
 }
