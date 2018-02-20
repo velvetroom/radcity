291195b176412331a1a75c2df874fbed6ab98933
Zero <zero.griffin@gmail.com>
2018-02-20 17:13:06 +0100

    Create layout size extension

--- Source/Extensions/ExtensionNSLayoutConstraint.swift
+++ Source/Extensions/ExtensionNSLayoutConstraint.swift
@@ -3,43 +3,7 @@ import UIKit
 extension NSLayoutConstraint
 {
     
-    @discardableResult class func height(
-        view:UIView,
-        constant:CGFloat = 0,
-        multiplier:CGFloat = 1) -> NSLayoutConstraint
-    {
-        let constraint:NSLayoutConstraint = NSLayoutConstraint(
-            item:view,
-            attribute:NSLayoutAttribute.height,
-            relatedBy:NSLayoutRelation.equal,
-            toItem:nil,
-            attribute:NSLayoutAttribute.notAnAttribute,
-            multiplier:multiplier,
-            constant:constant)
-        
-        constraint.isActive = true
-        
-        return constraint
-    }
     
-    @discardableResult class func heightGreaterOrEqual(
-        view:UIView,
-        constant:CGFloat = 0,
-        multiplier:CGFloat = 1) -> NSLayoutConstraint
-    {
-        let constraint:NSLayoutConstraint = NSLayoutConstraint(
-            item:view,
-            attribute:NSLayoutAttribute.height,
-            relatedBy:NSLayoutRelation.greaterThanOrEqual,
-            toItem:nil,
-            attribute:NSLayoutAttribute.notAnAttribute,
-            multiplier:multiplier,
-            constant:constant)
-        
-        constraint.isActive = true
-        
-        return constraint
-    }
     
     class func size(
         view:UIView,
@@ -57,22 +21,5 @@ extension NSLayoutConstraint
             multiplier:multiplier)
     }
     
-    @discardableResult class func height(
-        view:UIView,
-        toView:UIView,
-        multiplier:CGFloat = 1) -> NSLayoutConstraint
-    {
-        let constraint:NSLayoutConstraint = NSLayoutConstraint(
-            item:view,
-            attribute:NSLayoutAttribute.height,
-            relatedBy:NSLayoutRelation.equal,
-            toItem:toView,
-            attribute:NSLayoutAttribute.height,
-            multiplier:multiplier,
-            constant:0)
-        
-        constraint.isActive = true
-        
-        return constraint
-    }
+    
 }
