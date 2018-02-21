2b7dc336cc20de348ec6751383118939b90d7355
Zero <zero.griffin@gmail.com>
2018-02-21 08:16:36 +0100

    Create extension for transitionable default

--- Source/View/Global/View/ViewTransitionableProtocol.swift
+++ Source/View/Global/View/ViewTransitionableProtocol.swift
@@ -2,10 +2,13 @@ import UIKit
 
 protocol ViewTransitionableProtocol:class
 {
-    weak var layoutLeft:NSLayoutConstraint! { get set }
-    weak var layoutRight:NSLayoutConstraint! { get set }
-    weak var layoutTop:NSLayoutConstraint! { get set }
-    weak var layoutBottom:NSLayoutConstraint! { get set }
+    weak var view:UIView? { get }
+    
+    weak var layoutLeft:NSLayoutConstraint? { get set }
+    weak var layoutRight:NSLayoutConstraint? { get set }
+    weak var layoutTop:NSLayoutConstraint? { get set }
+    weak var layoutBottom:NSLayoutConstraint? { get set }
     weak var pushBackground:ViewPushBackground? { get set }
+    
     var shouldPanBack:Bool { get set }
 }
