ec1c53143505007e5603e65db8895bc8dcb9f639
Zero <zero.griffin@gmail.com>
2018-02-22 14:27:05 +0100

    Create controller protocol

--- Source/Model/Model/Model.swift
+++ Source/Model/Model/Model.swift
@@ -2,5 +2,9 @@ import Foundation
 
 internal class Model<SpecialisedArchitecture>:ArchitectureMember, ModelProtocol where SpecialisedArchitecture:Architecture
 {
+    func factoryController<SpecialisedArchitecture>() -> Controller<SpecialisedArchitecture> where SpecialisedArchitecture : Architecture {
+        return
+    }
+    
     internal typealias GenericArchitecture = SpecialisedArchitecture
 }
