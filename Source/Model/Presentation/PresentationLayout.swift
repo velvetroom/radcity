886401d5a0298f660e919e9e5c9f36504978a0bf
Zero <zero.griffin@gmail.com>
2018-02-22 16:50:01 +0100

    Create presentation layout struct

---
+++ Source/Model/Presentation/PresentationLayout.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+struct PresentationLayout
+{
+    weak var layoutTop:NSLayoutConstraint?
+    weak var layoutBottom:NSLayoutConstraint?
+    weak var layoutLeft:NSLayoutConstraint?
+    weak var layoutRight:NSLayoutConstraint?
+}
