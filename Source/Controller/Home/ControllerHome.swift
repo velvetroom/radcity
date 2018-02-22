a6cdb84634388281ea6ecac4f990d249233a65ff
Zero <zero.griffin@gmail.com>
2018-02-22 11:00:59 +0100

    Remove controller protocol

--- Source/Controller/Home/ControllerHome.swift
+++ Source/Controller/Home/ControllerHome.swift
@@ -2,5 +2,15 @@ import Foundation
 
 internal final class ControllerHome:Controller<ArchitectureHome>
 {
+    init()
+    {
+        super.init(
+            nibName:nil,
+            bundle:nil)
+    }
     
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
