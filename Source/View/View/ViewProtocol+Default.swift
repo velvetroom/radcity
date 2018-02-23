d0c17b8fc5158ea6085b8db6232de6324a0264ea
Zero <zero.griffin@gmail.com>
2018-02-23 08:40:35 +0100

    Factory view on default protocol

--- Source/View/View/ViewProtocol+Default.swift
+++ Source/View/View/ViewProtocol+Default.swift
@@ -3,4 +3,23 @@ import UIKit
 internal extension ViewProtocol
 {
     //MARK: internal
+    
+    internal static var view:UIView?
+    {
+        get
+        {
+            guard
+                
+                let type:UIView.Type = self as? UIView.Type
+                
+            else
+            {
+                return nil
+            }
+            
+            let view:UIView = type.init()
+            
+            return view
+        }
+    }
 }
