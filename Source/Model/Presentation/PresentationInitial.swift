2d434c7428f383e81785f1f0ec9ef33a22b546f1
Zero <zero.griffin@gmail.com>
2018-02-23 09:17:32 +0100

    Instantiate view

--- Source/Model/Presentation/PresentationInitial.swift
+++ Source/Model/Presentation/PresentationInitial.swift
@@ -2,7 +2,6 @@ import UIKit
 
 internal struct PresentationInitial:PresentationProtocol
 {
-    internal weak var view:UIView?    
     internal var controller:UIViewController?
     internal let model:ModelProtocol
     internal let presentationStrategy:((Presenter) -> (PresentationProtocol) -> ()) = Presenter.strategyInitial
--- Source/View/Home/ViewHome.swift
+++ Source/View/Home/ViewHome.swift
@@ -1,6 +1,12 @@
-import Foundation
+import UIKit
 
 internal final class ViewHome:View<ArchitectureHome>
 {
+    //MARK: internal
     
+    override func factoryViews()
+    {
+        super.factoryViews()
+        self.backgroundColor = UIColor.red
+    }
 }
