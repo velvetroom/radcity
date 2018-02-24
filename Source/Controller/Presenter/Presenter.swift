b449491c47591a9ccd6e5d0d3d147b8c136d3aef
Zero <zero.griffin@gmail.com>
2018-02-24 10:31:25 +0100

    Update format to allmans'

--- Source/Controller/Presenter/Presenter.swift
+++ Source/Controller/Presenter/Presenter.swift
@@ -1,27 +1,18 @@
 import UIKit
 
-internal final class Presenter:UIViewController, PresenterProtocol
-{
+internal final class Presenter:UIViewController, PresenterProtocol {
     internal var orientation:UIInterfaceOrientationMask
     
-    //MARK: internal
-    
-    internal init()
-    {
+    internal init() {
         self.orientation = UIInterfaceOrientationMask.portrait
-     
-        super.init(
-            nibName:nil,
-            bundle:nil)
+        super.init(nibName:nil, bundle:nil)
     }
     
-    internal required init?(coder:NSCoder)
-    {
+    internal required init?(coder:NSCoder) {
         return nil
     }
     
-    internal override func loadView()
-    {
+    internal override func loadView() {
         let viewPresenter:ViewPresenter = ViewPresenter()
         self.view = viewPresenter
     }
