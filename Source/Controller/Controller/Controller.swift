5c06b10e05e48e631d76a40fd0a4dc6fd7194d8d
Zero <zero.griffin@gmail.com>
2018-02-24 10:50:26 +0100

    Update using allmans'

--- Source/Controller/Controller/Controller.swift
+++ Source/Controller/Controller/Controller.swift
@@ -1,40 +1,22 @@
 import UIKit
 
 internal class Controller<SpecialisedArchitecture>:UIViewController,
-    ArchitectureMember, ControllerProtocol where SpecialisedArchitecture:Architecture
-{
+    ArchitectureMember, ControllerProtocol where SpecialisedArchitecture:Architecture {
     internal typealias GenericArchitecture = SpecialisedArchitecture
     
-    //MARK: internal
-    
-    internal init()
-    {
-        super.init(
-            nibName:nil,
-            bundle:nil)
+    internal init() {
+        super.init(nibName:nil, bundle:nil)
     }
     
-    internal required init?(coder:NSCoder)
-    {
+    internal required init?(coder:NSCoder) {
         return nil
     }
     
-    internal override func viewDidLoad()
-    {
+    internal override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    internal override func loadView()
-    {
-        guard
-            
-            let view:UIView = GenericArchitecture.GenericView.view
-        
-        else
-        {
-            return
-        }
-        
-        self.view = view
+    internal override func loadView() {
+        self.view = GenericArchitecture.GenericView.view
     }
 }
