c89811b3c3d622b9afb344f5a86fb8fa5d6d11e1
Zero <zero.griffin@gmail.com>
2018-02-24 10:27:34 +0100

    Update format using allmans'

--- Source/Extension/ExtensionUIView+LayoutVertical.swift
+++ Source/Extension/ExtensionUIView+LayoutVertical.swift
@@ -1,80 +1,61 @@
 import UIKit
 
-public extension UIView
-{
-    //MARK: public
-    
-    @discardableResult public func layoutTopToTop(
-        view:UIView,
-        constant:CGFloat = 0,
-        multiplier:CGFloat = 1) -> NSLayoutConstraint
-    {
+public extension UIView {
+    @discardableResult
+    public func layoutTopToTop(view:UIView, constant:CGFloat = 0) -> NSLayoutConstraint {
         let constraint:NSLayoutConstraint = NSLayoutConstraint(
             item:self,
             attribute:NSLayoutAttribute.top,
             relatedBy:NSLayoutRelation.equal,
             toItem:view,
             attribute:NSLayoutAttribute.top,
-            multiplier:multiplier,
+            multiplier:1,
             constant:constant)
-        
         constraint.isActive = true
         
         return constraint
     }
     
-    @discardableResult public func layoutTopToBottom(
-        view:UIView,
-        constant:CGFloat = 0,
-        multiplier:CGFloat = 1) -> NSLayoutConstraint
-    {
+    @discardableResult
+    public func layoutTopToBottom(view:UIView, constant:CGFloat = 0) -> NSLayoutConstraint {
         let constraint:NSLayoutConstraint = NSLayoutConstraint(
             item:self,
             attribute:NSLayoutAttribute.top,
             relatedBy:NSLayoutRelation.equal,
             toItem:view,
             attribute:NSLayoutAttribute.bottom,
-            multiplier:multiplier,
+            multiplier:1,
             constant:constant)
-        
         constraint.isActive = true
         
         return constraint
     }
     
-    @discardableResult public func layoutBottomToBottom(
-        view:UIView,
-        constant:CGFloat = 0,
-        multiplier:CGFloat = 1) -> NSLayoutConstraint
-    {
+    @discardableResult
+    public func layoutBottomToBottom(view:UIView, constant:CGFloat = 0) -> NSLayoutConstraint {
         let constraint:NSLayoutConstraint = NSLayoutConstraint(
             item:self,
             attribute:NSLayoutAttribute.bottom,
             relatedBy:NSLayoutRelation.equal,
             toItem:view,
             attribute:NSLayoutAttribute.bottom,
-            multiplier:multiplier,
+            multiplier:1,
             constant:constant)
-        
         constraint.isActive = true
         
         return constraint
     }
     
-    @discardableResult public func layoutBottomToTop(
-        view:UIView,
-        constant:CGFloat = 0,
-        multiplier:CGFloat = 1) -> NSLayoutConstraint
-    {
+    @discardableResult
+    public func layoutBottomToTop(view:UIView, constant:CGFloat = 0) -> NSLayoutConstraint {
         let constraint:NSLayoutConstraint = NSLayoutConstraint(
             item:self,
             attribute:NSLayoutAttribute.bottom,
             relatedBy:NSLayoutRelation.equal,
             toItem:view,
             attribute:NSLayoutAttribute.top,
-            multiplier:multiplier,
+            multiplier:1,
             constant:constant)
-        
         constraint.isActive = true
         
         return constraint
