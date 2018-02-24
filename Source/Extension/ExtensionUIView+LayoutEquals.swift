b67b440a56aeaa53b42c5528b0690d77d1a98e78
Zero <zero.griffin@gmail.com>
2018-02-24 10:14:50 +0100

    Reformat using allman's

--- Source/Extension/ExtensionUIView+LayoutEquals.swift
+++ Source/Extension/ExtensionUIView+LayoutEquals.swift
@@ -1,50 +1,20 @@
 import UIKit
 
-public extension UIView
-{
-    //MARK: public
-    
-    public func layoutEquals(
-        view:UIView,
-        margin:CGFloat = 0)
-    {
-        self.layoutTopToTop(
-            view:view,
-            constant:margin)
-        self.layoutBottomToBottom(
-            view:view,
-            constant:-margin)
-        self.layoutLeftToLeft(
-            view:view,
-            constant:margin)
-        self.layoutRightToRight(
-            view:view,
-            constant:-margin)
+public extension UIView {
+    public func layoutEquals(view:UIView, margin:CGFloat = 0) {
+        self.layoutTopToTop(view:view, constant:margin)
+        self.layoutBottomToBottom(view:view, constant:-margin)
+        self.layoutLeftToLeft(view:view, constant:margin)
+        self.layoutRightToRight(view:view, constant:-margin)
     }
     
-    public func layoutEqualsHorizontal(
-        view:UIView,
-        margin:CGFloat = 0)
-    {
-        self.layoutLeftToLeft(
-            view:view,
-            constant:margin)
-        
-        self.layoutRightToRight(
-            view:view,
-            constant:-margin)
+    public func layoutEqualsHorizontal(view:UIView, margin:CGFloat = 0) {
+        self.layoutLeftToLeft(view:view, constant:margin)
+        self.layoutRightToRight(view:view, constant:-margin)
     }
     
-    public func layoutEqualsVertical(
-        view:UIView,
-        margin:CGFloat = 0)
-    {
-        self.layoutTopToTop(
-            view:view,
-            constant:margin)
-        
-        self.layoutBottomToBottom(
-            view:view,
-            constant:-margin)
+    public func layoutEqualsVertical(view:UIView, margin:CGFloat = 0) {
+        self.layoutTopToTop(view:view, constant:margin)
+        self.layoutBottomToBottom(view:view, constant:-margin)
     }
 }
