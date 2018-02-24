008f25b0430488d8221c2eac9a3dfd68e34b83fc
Zero <zero.griffin@gmail.com>
2018-02-24 10:45:13 +0100

    Update using allmans'

--- Source/Controller/Presenter/Presenter+Strategies.swift
+++ Source/Controller/Presenter/Presenter+Strategies.swift
@@ -1,28 +1,17 @@
 import UIKit
 
-internal extension Presenter
-{
-    //MARK: internal
-    
-    internal func strategyInitial(presentation:PresentationProtocol)
-    {
+internal extension Presenter {
+    internal func strategyInitial(presentation:PresentationProtocol) {
         self.addView(presentation:presentation)
         self.layoutStrategyInitial(presentation:presentation)
     }
     
-    //MARK: private
-    
-    private func layoutStrategyInitial(presentation:PresentationProtocol)
-    {
+    private func layoutStrategyInitial(presentation:PresentationProtocol) {
         guard
-            
             var view:ViewProtocol = presentation.view as? ViewProtocol
-            
-        else
-        {
+        else {
             return
         }
-        
         view.presentationLayout.layoutTop = presentation.view?.layoutTopToTop(view:self.view)
         view.presentationLayout.layoutBottom = presentation.view?.layoutBottomToBottom(view:self.view)
         view.presentationLayout.layoutRight = presentation.view?.layoutRightToRight(view:self.view)
