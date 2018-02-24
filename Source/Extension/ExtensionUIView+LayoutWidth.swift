9fd23cf6b6fecee02b169b7ec2dd9878df98a6ab
Zero <zero.griffin@gmail.com>
2018-02-24 10:29:46 +0100

    Update using allmans'

--- Source/Extension/ExtensionUIView+LayoutWidth.swift
+++ Source/Extension/ExtensionUIView+LayoutWidth.swift
@@ -1,58 +1,46 @@
 import UIKit
 
-public extension UIView
-{
-    //MARK: public
-    
-    @discardableResult public func layoutWidth(
-        constant:CGFloat = 0,
-        multiplier:CGFloat = 1) -> NSLayoutConstraint
-    {
+public extension UIView {
+    @discardableResult
+    public func layoutWidth(constant:CGFloat = 0) -> NSLayoutConstraint {
         let constraint:NSLayoutConstraint = NSLayoutConstraint(
             item:self,
             attribute:NSLayoutAttribute.width,
             relatedBy:NSLayoutRelation.equal,
             toItem:nil,
             attribute:NSLayoutAttribute.notAnAttribute,
-            multiplier:multiplier,
+            multiplier:1,
             constant:constant)
-        
         constraint.isActive = true
         
         return constraint
     }
     
-    @discardableResult public func layoutWidthGreaterOrEqual(
-        constant:CGFloat = 0,
-        multiplier:CGFloat = 1) -> NSLayoutConstraint
-    {
+    @discardableResult
+    public func layoutWidthGreaterOrEqual(constant:CGFloat = 0) -> NSLayoutConstraint {
         let constraint:NSLayoutConstraint = NSLayoutConstraint(
             item:self,
             attribute:NSLayoutAttribute.width,
             relatedBy:NSLayoutRelation.greaterThanOrEqual,
             toItem:nil,
             attribute:NSLayoutAttribute.notAnAttribute,
-            multiplier:multiplier,
+            multiplier:1,
             constant:constant)
-        
         constraint.isActive = true
         
         return constraint
     }
     
-    @discardableResult public func layoutWidth(
-        view:UIView,
-        multiplier:CGFloat = 1) -> NSLayoutConstraint
-    {
+    @discardableResult
+    public func layoutWidth(view:UIView) -> NSLayoutConstraint {
         let constraint:NSLayoutConstraint = NSLayoutConstraint(
             item:self,
             attribute:NSLayoutAttribute.width,
             relatedBy:NSLayoutRelation.equal,
             toItem:view,
             attribute:NSLayoutAttribute.width,
-            multiplier:multiplier,
+            multiplier:1,
             constant:0)
-        
         constraint.isActive = true
         
         return constraint
